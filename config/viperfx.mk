01a6207e9e8bb427e8b624900cdfc4438e8447d8
kallop <kallop2016@163.com>
2016-03-28 02:10:39 +0800

    vendor: fix bulid v4a
    Change-Id: I58eddf848a17c37bc983a51946dff47009590bc3

--- config/viperfx.mk
+++ config/viperfx.mk
@@ -1,5 +1,8 @@
 PRODUCT_PACKAGES += \
-    Viper4Android
+    v4a
+
+PRODUCT_COPY_FILES += \
+     packages/apps/v4a/assets/libv4a_fx_ics.so:system/lib/soundfx/libv4a_fx_ics.so
 
 PRODUCT_PROPERTY_OVERRIDES += \
     lpa.decode=false \
